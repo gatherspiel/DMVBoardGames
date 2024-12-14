c0628cf6ebd16e03aea0b91ec34e60c8a04b9144
Bharat Ponnaluri <gulu@createthirdplaces.com>
2024-12-14 15:38:30 -0500

    Updates

---
+++ src/data/GroupData.js
@@ -0,0 +1,86 @@
+const GROUP_DATA = [];
+
+const EVENT_1 = {
+  title: "High Interaction Board Games at Western Market Food Hall in DC",
+  day: "Sunday",
+  location: "2000 Pennsylvania Avenue NW #3500 · Washington, DC",
+  summary:
+    "We play a variety of high interaction games with a focus on cooperative games, hidden identity games, and high interaction(German-style) Euros.",
+};
+
+const EVENT_2 = {
+  title: "Board Game Night @ Board Room in Clarendon, Wed, 6:30-10:00",
+  day: "Wednesday",
+  location: "925 N Garfield St · Arlington, VA",
+  summary:
+    "We play a variety of high interaction games with a focus on cooperative games, hidden identity games, and high interaction(German-style) Euros.",
+};
+
+const EVENT_3 = {
+  title: "Bring Your Own Game Night in DC at Nanny O’Briens",
+  day: "Monday",
+  location: "3319 Connecticut Avenue Northwest · Washington, DC",
+  summary:
+    "We play a variety of high interaction games with a focus on cooperative games, hidden identity games, and high interaction(German-style) Euros.",
+};
+
+const GROUP_1 = {
+  link: "https://www.meetup.com/beerbogglers/",
+  events: [EVENT_1, EVENT_2, EVENT_3],
+  title: "Beer & Board Games",
+  summary:
+    "Hi! This is a group for fun-loving, non-competitive people who enjoy playing party and board games. We have several wonderful venues where we can enjoy food and drinks while playing a few games. Don’t hesitate to bring any games you'd like to play (or feel free to come empty-handed).",
+};
+
+const GROUP_2 = {
+  link: "https://www.meetup.com/nontraditional-board-games/",
+  events: [
+    {
+      title: "Board Games @ Third Hill Brewing Co.",
+      day: "Tuesday",
+      location: "8216 Georgia Ave · Silver Spring, MD",
+      summary:
+        "Come out to Third Hill Brewing Company and play some board games! Serving food with their variety of drinks so come and fuel up for the games.\n" +
+        "\n",
+    },
+    {
+      title: "Board Games @ Silver Spring Library",
+      day: "Saturday",
+      location: "900 Wayne Ave · Silver Spring, MD",
+      summary:
+        "Grab a game or bring your own, this event is open play. Whether it is chucking dice, full of traitors, laying tiles, or only cards involved, it doesn't matter; as long as it is a fun game, let's get it to the table and play it.",
+    },
+  ],
+  title: "NonTraditional Board Games",
+  summary:
+    "We are all about board games here. NonTraditional Board Games (NTBoardGames for short) is a group for people to come together, play board games, have fun, and meet new friends.\n" +
+    "\n" +
+    "There's nothing wrong with Monopoly or Scattergories, but this group is focused on games you might've not heard before. Some of the games you can expect to play are Lords of Xidit, Tortuga 1667, Eldritch Horror, and many more.\n" +
+    "\n" +
+    "Let's Play Some Games!",
+};
+
+const GROUP_3 = {
+  link: "https://www.meetup.com/board-games-at/",
+  events: [
+    {
+      title: "Game Night at Glory Days",
+      day: "Monday",
+      location: "3141 Duke Street · Alexandria, VA",
+      summary:
+        "We will be playing board games at Glory Days Grill in Alexandria . We don’t play typical games like scrabble or monopoly. We play Euro games like ticket to ride, Lords of Waterdeep, Wingspan, Cascadia and more.",
+    },
+  ],
+  title: "NonTraditional Board Games",
+  summary:
+    "We are all about board games here. NonTraditional Board Games (NTBoardGames for short) is a group for people to come together, play board games, have fun, and meet new friends.\n" +
+    "\n" +
+    "There's nothing wrong with Monopoly or Scattergories, but this group is focused on games you might've not heard before. Some of the games you can expect to play are Lords of Xidit, Tortuga 1667, Eldritch Horror, and many more.\n" +
+    "\n" +
+    "Let's Play Some Games!",
+};
+
+const CONVENTION_1 = {
+  title: "Congress of Gamers Winter Session",
+  days: ["1/18/2025", "1/19/2025"],
+};
