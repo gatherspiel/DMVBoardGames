17a172be92ba66ad41c2286e5b5de0faa8334c4b
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-01-11 18:45:55 -0500

    Updates

--- src/events/scripts/eventListing.js
+++ src/events/scripts/eventListing.js
@@ -1,34 +1,82 @@
 import { GROUP_DATA } from "./GroupData.js";
 
-const stateData = {};
+const groupState = {};
 
-function buttonClickHandler(evt) {
-  console.log("Testing");
-  console.log(evt);
+function getGroupData(groupId) {
+  return groupState[groupId]["groupData"];
+}
+
+function updateGroupVisibilityState(groupId) {
+  if (isVisible(groupId)) {
+    groupState[groupId]["frontendState"].isVisible = false;
+  } else {
+    groupState[groupId]["frontendState"].isVisible = true;
+  }
+}
+
+function isVisible(groupId) {
+  const groupData = groupState[groupId]["frontendState"];
+  return groupData && groupData.isVisible;
+}
+
+function showHideHandler(groupId) {
+  updateGroupVisibilityState(groupId);
+
+  const groupElement = document.querySelector("#" + groupId);
+  groupElement.innerHTML = getGroupHtml(getGroupData(groupId));
+  addEventHandler(groupElement);
+}
+
+function addEventHandler(groupElement) {
+  const groupId = groupElement.id;
+  const showHideButton = groupElement.querySelector(".show-hide-button");
+
+  showHideButton.addEventListener("click", () => showHideHandler(groupId));
+}
+
+function setupEventHandlers() {
+  const groups = document.querySelectorAll(".event-group");
+  groups.forEach((group) => {
+    addEventHandler(group);
+  });
+}
+
+function getGroupHtml(group) {
+  let groupHtml = "";
+  const groupId = "group-" + group.id;
+  groupHtml = `
+      <div id=${groupId} class=${"event-group"}>
+        <h2>
+          <a href=${group.link}>${group.title}</a>
+          <span>:&nbsp;${group.locations.replaceAll(",", ", ")}</span>
+        </h2>  
+        
+        <button class='show-hide-button'>
+          ${isVisible(groupId) ? "Hide info" : "Show info"}
+        </button>
+
+      </div>
+    `;
+  return groupHtml;
 }
 
 function displayListing(groupData) {
   console.log(groupData);
   let html = "";
   Object.values(groupData).forEach((group) => {
-    let groupHtml = "";
-    groupHtml = `
-      <div id=${group.id}>
-        <h2>
-          <a href=${group.link}>${group.title}</a>
-          <span>:&nbsp;${group.locations.replaceAll(",", ", ")}</span>
-        </h2>  
-        
-        <button onClick="${buttonClickHandler}">
-          Click
-        </button>
+    groupState["group-" + group.id] = {
+      groupData: group,
+      frontendState: {},
+    };
 
-      </div>
-    `;
+    let groupHtml = "";
+    groupHtml = getGroupHtml(group);
     html += groupHtml;
   });
 
   document.querySelector("#listing").innerHTML = html;
 }
+
 console.log("Setting up event listing");
 displayListing(GROUP_DATA);
+setupEventHandlers();
