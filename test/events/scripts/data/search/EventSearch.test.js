feef3c68022124593d72198a300618d8f90cd5e7
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-02-01 21:15:24 -0500

    Update

--- test/events/scripts/data/search/EventSearch.test.js
+++ test/events/scripts/data/search/EventSearch.test.js
@@ -9,6 +9,7 @@ import { DEFAULT_SEARCH_PARAMS, GROUP_LIST_1 } from "./Fixtures.js";
 describe("Event Search tests", () => {
   it("Should return empty result when there are no groups", () => {
     const results = findResults({}, DEFAULT_SEARCH_PARAMS);
+    //TODO: Add assert statement.
   });
 
   it("Should return all events when the default search parameters are specified", () => {
