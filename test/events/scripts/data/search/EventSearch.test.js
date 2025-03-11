7fff60bea1be87db535eab9deb23d4940f9a79f9
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-03-11 17:45:52 -0400

    Updates

--- test/events/scripts/data/search/EventSearch.test.js
+++ test/events/scripts/data/search/EventSearch.test.js
@@ -1,10 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import {
-  countEvents,
-  getEventIds,
-  getSearchResultGroups,
-} from "../../../../../src/events/scripts/data/search/search.js";
+import { getSearchResultGroups } from "../../../../../src/events/scripts/data/search/EventSearch.js";
 import { intersection } from "../../../../testUtils.js";
 import {
   ARLINGTON_SEARCH,
@@ -14,6 +10,7 @@ import {
   INVALID_DAY_SEARCh,
   WEDNESDAY_SEARCH,
 } from "./Fixtures.js";
+import { SearchResult } from "../../../../../src/events/scripts/data/search/model/SearchResult.js";
 
 describe("Event Search tests", () => {
   it("Should return empty result when there are no groups", () => {
@@ -24,14 +21,15 @@ describe("Event Search tests", () => {
   it("Should return all events when the default search parameters are specified", () => {
     const groups = getSearchResultGroups(GROUP_LIST_1, DEFAULT_SEARCH_PARAMS);
     assert.strictEqual(groups.length, 3, JSON.stringify(groups));
+    console.log(groups);
     assert.strictEqual(
-      countEvents(groups),
+      SearchResult.countEvents(groups),
       2,
       "Number of events is not correct",
     );
 
     const expectedIds = new Set([1, 2]);
-    const eventIds = getEventIds(groups);
+    const eventIds = SearchResult.getEventIds(groups);
     assert.strictEqual(intersection(expectedIds, eventIds).length, 2);
   });
 
@@ -39,13 +37,13 @@ describe("Event Search tests", () => {
     const groups = getSearchResultGroups(GROUP_LIST_2, ARLINGTON_SEARCH);
     assert.strictEqual(groups.length, 7, JSON.stringify(groups));
     assert.strictEqual(
-      countEvents(groups),
+      SearchResult.countEvents(groups),
       4,
       "Number of events is not correct",
     );
 
     const expectedIds = new Set([1, 2, 4, 6]);
-    const eventIds = getEventIds(groups);
+    const eventIds = SearchResult.getEventIds(groups);
     assert.strictEqual(
       intersection(expectedIds, eventIds).length,
       4,
@@ -57,14 +55,14 @@ describe("Event Search tests", () => {
     const groups = getSearchResultGroups(GROUP_LIST_2, WEDNESDAY_SEARCH);
     assert.strictEqual(groups.length, 5, JSON.stringify(groups));
     assert.strictEqual(
-      countEvents(groups),
+      SearchResult.countEvents(groups),
       6,
       "Number of events is not correct",
     );
 
     const expectedIds = new Set([4, 1, 3, 6, 8, 10]);
 
-    const eventIds = getEventIds(groups);
+    const eventIds = SearchResult.getEventIds(groups);
     assert.strictEqual(
       intersection(expectedIds, eventIds).length,
       6,
@@ -77,7 +75,7 @@ describe("Event Search tests", () => {
     const groups = getSearchResultGroups(GROUP_LIST_2, INVALID_DAY_SEARCh);
     assert.strictEqual(groups.length, 0, JSON.stringify(groups));
     assert.strictEqual(
-      countEvents(groups),
+      SearchResult.countEvents(groups),
       0,
       "Number of events is not correct",
     );
