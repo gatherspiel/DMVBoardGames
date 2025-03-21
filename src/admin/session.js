8907e1cc233e7c090748b5b4992f53a246a06c1d
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-03-20 21:32:49 -0400

    Updates

--- src/admin/session.js
+++ src/admin/session.js
@@ -5,25 +5,42 @@ const supabase = createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthcnF5c2t1dWRudmZ4b2h3a29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5ODQ5NjgsImV4cCI6MjA1NzU2MDk2OH0.TR-Pn6dknOTtqS9y-gxK_S1-nw6TX-sL3gRH2kXJY_I",
 );
 
+const SERVER_URL = "http://localhost:7070/";
+const ENDPOINT = "admin/saveData";
 window.onload = function () {
   let form = document.getElementById("loginForm");
 
-  async function login() {
+  async function saveData() {
     console.log(supabase);
     const email = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const data = document.getElementById("data").value;
 
-    console.log(email);
-    console.log(password);
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const url = SERVER_URL + ENDPOINT;
+    try {
+      console.log("Attempting to save data");
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          data: data,
+        }),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    const { authData, authError } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
     });
 
-    console.log(data);
+    console.log(authData);
   }
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    login();
+    saveData();
   });
 };
