182ca42e00f12111162e1005d4bdeda0ed103e92
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-05-09 00:25:15 -0400

    Fix API url for prod

--- src/utils/params.js
+++ src/utils/params.js
@@ -1,5 +1,5 @@
 export const API_ROOT = window.location.href.includes(
   "https://dmvboardgames.com/",
 )
-  ? `https://${import.meta.env.VITE_API_ROOT}`
+  ? `${import.meta.env.VITE_API_ROOT}`
   : import.meta.env.VITE_LOCAL_API_ROOT;
