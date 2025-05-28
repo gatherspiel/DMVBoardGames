2da94b0c9cda0c365eb9283ea4a447030b6f689e
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-05-28 01:01:21 -0400

    Updates

--- src/framework/store/update/api/BaseGetAction.ts
+++ src/framework/store/update/api/BaseGetAction.ts
@@ -2,21 +2,42 @@ import type { DefaultApiAction } from "./DefaultApiAction.ts";
 import { BaseThunkAction } from "../BaseThunkAction.ts";
 import { getResponseData } from "../../data/RequestStore.ts";
 
+export const ApiActionTypes = Object.freeze({
+  GET: "GET",
+});
+
+export type ApiRequestCofnig = {
+  url: string;
+  method: typeof ApiActionTypes;
+  headers?: Record<string, string>;
+  body?: any;
+};
 export class BaseGetAction extends BaseThunkAction {
-  mockResponse: DefaultApiAction;
+  defaultResponse: DefaultApiAction;
   getQueryUrl: (a: any) => string;
 
-  constructor(getQueryUrl: (a: any) => string, mockResponse: DefaultApiAction) {
+  constructor(
+    getQueryUrl: (a: any) => string,
+    defaultResponse: DefaultApiAction,
+  ) {
     super();
     this.getQueryUrl = getQueryUrl;
-    this.mockResponse = mockResponse;
+    this.defaultResponse = defaultResponse;
   }
 
+  /**
+   * TODO:
+   * - Rename getQueryUrl to getQueryConfig
+   * - Rename this class to BaseApiAction.ts
+   * - Make sure retrieveData sets the token before sending the request
+   * @param params
+   */
   async retrieveData(params: any): Promise<any> {
     const baseGet: BaseGetAction = this;
+
     return await getResponseData(
       baseGet.getQueryUrl(params),
-      baseGet.mockResponse,
+      baseGet.defaultResponse,
     );
   }
 }
