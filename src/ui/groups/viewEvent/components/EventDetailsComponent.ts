196ca1d260d753828e0ac6e44302a14266b75c45
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-06-28 03:11:03 -0400

    Make sure event pages retrieve data from the API, and fix issues with navigation and loading CSS

--- src/ui/groups/viewEvent/components/EventDetailsComponent.ts
+++ src/ui/groups/viewEvent/components/EventDetailsComponent.ts
@@ -1,33 +1,57 @@
-import { BaseTemplateDynamicComponent } from "../../../../framework/components/BaseTemplateDynamicComponent.ts";
-
+import {BaseTemplateDynamicComponent} from "../../../../framework/components/BaseTemplateDynamicComponent.ts";
+import {GET_EVENT_REQUEST_STORE, GROUP_NAME_PARAM} from "../../Constants.ts";
+import {getUrlParameter} from "../../../../framework/utils/UrlParamUtils.ts";
+import {EVENT_REQUEST_THUNK} from "../data/EventRequestThunk.ts";
 
 const template = `
   <link rel="stylesheet" type="text/css" href="/styles/sharedComponentStyles.css"/>
-
-  <style>
-
-    
+  <style>   
   </style>
 `;
 
+
+const loadConfig = {
+  onLoadStoreConfig: {
+    storeName: GET_EVENT_REQUEST_STORE,
+    dataSource: EVENT_REQUEST_THUNK,
+  },
+  onLoadRequestData: {
+    name: getUrlParameter(GROUP_NAME_PARAM),
+  },
+  thunkReducers: [
+    {
+      thunk: EVENT_REQUEST_THUNK,
+      componentStoreReducer: function (data: any) {
+        console.log(JSON.stringify(data));
+        return data;
+      },
+    }
+  ],
+  globalStateLoadConfig: {
+    globalFieldSubscriptions: ["isLoggedIn"],
+    waitForGlobalState: "isLoggedIn",
+  },
+};
+
+
 export class EventDetailsComponent extends BaseTemplateDynamicComponent {
   constructor() {
-    super("group-event-component");
+    super("group-event-component", loadConfig);
   }
 
   connectedCallback() {
     this.updateStore({});
   }
 
-  render(): string {
-    console.log("Hi");
+  render(data: any): string {
+    console.log(JSON.stringify(data));
 
     return `
       <div class="ui-section">
-         <h1>View group event</h1>
-      <button>Edit event</button>  
-      <button>Back to group</button> 
-</div>
+        <h1>View group event</h1>
+        <button>Edit event</button>  
+        <button>Back to group</button> 
+      </div>
  
     `;
   }
