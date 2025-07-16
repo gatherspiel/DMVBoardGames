2fab98c99f36de091402e783bb9769c212df3a1f
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-07-15 22:31:44 -0400

    Remove extra form type

--- src/framework/components/types/FormItemConfig.ts
+++ src/framework/components/types/FormItemConfig.ts
@@ -6,13 +6,3 @@ export type FormItemConfig = {
     className?:string,
     lineBreakAfterLabel?: boolean,
 }
-
-
-export type FormConfig = {
-    componentLabel: string,
-    id: string,
-    fieldName: string //If the value is undefined, a previous input value will be used if it is present.
-    inputType: string,
-    className?:string,
-    lineBreakAfterLabel?: boolean,
-}
