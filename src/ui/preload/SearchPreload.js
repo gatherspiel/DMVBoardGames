39d283d1f858c00971ac1e45a1ae228fa8fceed9
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-07-17 05:22:16 -0400

    Updates

--- src/ui/preload/SearchPreload.js
+++ src/ui/preload/SearchPreload.js
@@ -9,9 +9,13 @@ if(!IS_LOCAL){
   API_ROOT = "https://api.dmvboardgames.com"
 }
 
-fetch(`${API_ROOT}/searchEvents`).then((response)=>{
+console.log("Hi");
+const start = Date.now();
+fetch(`${API_ROOT}/searchEvents`,{priority: 'high'}).then((response)=>{
+  console.log(Date.now()-start);
   return response.json()
 }).then((response)=>{
   //@ts-ignore
   window.preloadData = response;
 })
+
