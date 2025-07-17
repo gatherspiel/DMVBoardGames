1bddfcc414f693ecbb441a5c4a522218dd5a2720
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-07-17 02:08:44 -0400

    Fix build issue

--- src/ui/preload/SearchPreload.ts
+++ src/ui/preload/SearchPreload.ts
@@ -3,7 +3,11 @@ const IS_LOCAL = !window.location.href.includes(
   "https://dmvboardgames.com/",
 );
 
-const API_ROOT = IS_LOCAL ? "http://localhost:7070" : "https://api.dmvboardgames.com/"
+let API_ROOT = "http://localhost:7070"
+
+if(!IS_LOCAL){
+  API_ROOT = "https://api.dmvboardgames.com"
+}
 
 fetch(`${API_ROOT}/searchEvents`).then((response)=>{
   return response.json()
