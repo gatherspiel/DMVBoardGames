9b3497538d1a263946bd6bdba1fabc5e62ae8785
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-10-07 22:47:35 -0400

    Remove bottom navigation bar and add other links section

--- src/shared/html/LoginStatusComponent.ts
+++ src/shared/html/LoginStatusComponent.ts
@@ -23,26 +23,19 @@ export class LoginStatusComponent extends BaseDynamicComponent {
           font-weight: 400;
           text-decoration: none;
         }
-        span {
-          font-weight: 400;
-        } 
         a:hover {
           background-color: var(--clr-darker-blue);
         }
         div {
-          padding-top:0.5rem;
-          font-size: 1.5rem;
-          color:var(--clr-darker-blue)
+          padding-top:0.25rem;
+        }
+        span {
+          font-weight: 400;
         } 
-        button {
-          font-size: 1.5rem;
-        }
-        button:active {
-          background-color: var(--clr-darker-blue) !important;
-        }
-        @media not screen and (width < 32em) {
+        
+        @media screen and (width < 32em) {    
           div {
-            padding-left:1rem;
+            padding-bottom: 0.5rem;
           }
         }
       </style>
