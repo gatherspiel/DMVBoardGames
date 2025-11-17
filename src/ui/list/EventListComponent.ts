3a49814cc107fe5535c7d72933af715e8ac866c9
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-11-17 04:21:35 -0500

    Update loading animiation

--- src/ui/list/EventListComponent.ts
+++ src/ui/list/EventListComponent.ts
@@ -55,83 +55,102 @@ export class EventListComponent extends BaseDynamicComponent {
   }
 
 
-
   override getTemplateStyle(): string {
     return `
       <link rel="preload" as="style" href="/styles/sharedHtmlAndComponentStyles.css" onload="this.rel='stylesheet'"/>
       <style>
-      
-    
-      #meeple-one {
-        position: absolute;
-        top:50px;
-        left:25px;
+  
+        #meeple-one {
+          position: absolute;
+          height:4px;
+          width:4px;
+          top:50px;
+          left:25px;
+        }
+        #meeple-two {
+          position: absolute;
+          top:43px;
+          left:43px;
+        }
+        #meeple-three {
+          position: absolute;
+          top:25px;
+          left:50px;
+        }
+        #meeple-four {
+          position: absolute;
+          top:7px;
+          left:43px;
+        }
+        #meeple-five {
+          position: absolute;
+          top:0;
+          left:25px;
+        }  
+        #meeple-six {
+          position: absolute;
+          top:7px;
+          left:7px;
+        }  
+        #meeple-seven {
+          position: absolute;
+          top:25px;
+          left:0;
+        }
+        #meeple-seven img {
+          height:3px;
+          width:3px;
+        }
+        #meeple-six img {
+          height:5px;
+          width:5px;
+        }
+        #meeple-five img {
+          height:7px;
+          width:7px;
+        }
+        #meeple-four img {
+          height:9px;
+          width:9px;
+        }
+        #meeple-three img {
+          height:11px;
+          width:11px;
+        }
+        #meeple-two img {
+          height:13px;
+          width:13px;
+        }
+        #meeple-one img {
+          height:15px;
+          width:15px;
+        }
+        .loader {
+          animation: l27 1s infinite steps(8);
+          height: 65px;
+          margin-left:auto;
+          margin-right:auto;
+          margin-top:2rem;
+          width: 65px;
       }
-      
-      #meeple-two {
-        position: absolute;
-        top:43px;
-        left:43px;
+      @keyframes l27 {
+        100% {
+        transform-origin: 32.5px 32.5px;
+        transform: rotate(1turn)
+        }
       }
-      
-      #meeple-three {
-        position: absolute;
-        top:25px;
-        left:50px;
+      .meeple {
+        width:15px;
+        height:15px;
+        animation: l28 1s infinite steps(8);
       }
-      
-      #meeple-four {
-        position: absolute;
-        top:7px;
-        left:43px;
+      @keyframes l28 {
+        100% {
+        transform-origin: 7.5px 7.5px;
+        transform: rotate(-1turn)
+        }
       }
-      
-      #meeple-five {
-        position: absolute;
-        top:0;
-        left:25px;
-      }
-      
-      #meeple-six {
-        position: absolute;
-        top:7px;
-        left:7px;
-      }
-      
-       #meeple-seven {
-        position: absolute;
-        top:25px;
-        left:0;
-      }
-      
-     
-      /* HTML:  */
-/* HTML: <div class="loader"></div> */
-    .loader {
-      margin-top:5rem;
-      width: 65px;
-      height: 65px;
-      animation: l27 1s infinite steps(8);
-    }
-    @keyframes l27 {
-      100% {
-      transform-origin: 32.5px 32.5px;
-      transform: rotate(1turn)
-      }
-    }
-    .meeple {
-      width:15px;
-      height:15px;
-      animation: l28 1111s infinite steps(8);
-    }
-    @keyframes l28 {
-      100% {
-      transform-origin: 7.5px 7.5px;
-      transform: rotate(-1turn)
-      }
-    }
    
-
        li {
           padding-bottom: 1rem;
           padding-top:1rem;
