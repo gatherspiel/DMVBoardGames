7f4be9586f8a04ee0b20d7beec886ad6b9f22a41
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-11-17 23:49:40 -0500

    Updates

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,8 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts}'],
+    extends: [js.configs.recommended],
+    files: ['**/*.{js}'],
     languageOptions: {
       ecmaVersion: 2021,
       globals: globals.browser,
