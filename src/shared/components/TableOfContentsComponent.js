9f28e77d7dbeeb4a0278b3cd9dff74f7eaa1fc5e
Bharat Ponnaluri <gulu@createthirdplaces.com>
2025-11-25 04:46:12 -0500

    Add table of contents

---
+++ src/shared/components/TableOfContentsComponent.js
@@ -0,0 +1,46 @@
+export class TableOfContentsComponent extends HTMLElement{
+
+  connectedCallback(){
+
+    let tableOfContents = `<h2>Table of contents</h2>`;
+
+    let count = 0;
+    this.querySelectorAll(".section-1").forEach(element=>{
+      element.id = "header-"+count;
+
+      const title = element.querySelector("h2").textContent;
+
+      let sectionHtml = ``;
+      let sectionCount = 0;
+      element.querySelectorAll("h3").forEach(sectionElement=>{
+
+        sectionElement.id = "section-header-" + sectionCount;
+
+        sectionHtml+=`
+          <li>
+            <a href="#${sectionElement.id}">${sectionElement.textContent}</a></br>
+          </li>
+        `
+        sectionCount ++;
+      })
+
+      console.log(sectionHtml);
+
+      if(sectionHtml.length !==0){
+        tableOfContents += `
+          <details>
+          <summary><a href="#${element.id}">${title}</a><br></summary>
+            <ul>
+              ${sectionHtml}
+            </ul>
+          </details>
+        `
+      } else {
+        tableOfContents += `<a href="#${element.id}">${title}</a><br>`
+      }
+
+      count++;
+    })
+    this.innerHTML = tableOfContents + this.innerHTML;
+  }
+}
