25e7d12e69a42ca0c89951b27d1926a1dbc1366a
Bharat Ponnaluri <gulu@createthirdplaces.com>
2026-07-18 22:21:46 -0400

    Update linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import globals from "globals";
 import js from "@eslint/js";
 
 export default defineConfig([
-  globalIgnores(["src/beta","src/shared/highlightjs"]),
+  globalIgnores(["src/beta","src/shared/highlightjs","src/lib/places-js-latest.js"]),
   { files: ["**/*.js"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
 
