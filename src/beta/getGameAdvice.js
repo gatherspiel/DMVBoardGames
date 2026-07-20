dbdabb3725f87712f376daae847a7ca7555f5e39
Gulu <gulu@createthirdplaces.com>
2026-07-20 18:51:15 +0200

    Update src/beta/getGameAdvice.js

--- src/beta/getGameAdvice.js
+++ src/beta/getGameAdvice.js
@@ -1,63 +1,51 @@
-
 const blockFunction = function(...args){
-	console.error("Unauthorized");
-}
-
-function blockObjectMethods() {
-	Object.keys = blockFunction;
-	Object.values = blockFunction;
-	Object.getOwnPropertyNames = blockFunction; 
+  console.error("Unauthorized");
 }
 
 export function GetGameAdvice(){
 
-    blockObjectMethods();
-    setTimeout( ()=>{   
-   
-			const a = document; 
-			const potato = "querySelector";
-			const name = "getElementById";
-		 
-			document.querySelector = function (...args) {
-				console.error("Unauthorized");
-			};
-
-			document.getElementById = function (...args) {
-				console.error("Unauthorized");   
-			};
-
-
-			document.elementFromPoint = function (...args) {
-				console.error("Unauthorized");  
-			};
-
-
-			document.appendChild = function (...args) { 
-				console.error("Unauthorized");  
-			};
-
-
-			document.getElementsByTagName = function (...args) {
-				console.error("Unauthorized");  
-			};
-
-
-			//Headless browsers.
-			navigator.permissions
-				.query({ name: "notifications" })
-				.then(function (permissionStatus) {
-					if (
-						Notification.permission === "denied" &&
-						permissionStatus.state === "prompt"
-					) {
-				    console.error("Unauthorized");	
-					}
-				});
-
-			window.innerWidth = 22;
-			window.innerHeight = 33;
-			window.devicePixelRatio = 2;
-		},2000)
+  setTimeout( ()=>{   
+ 
+    const a = document; 
+    const potato = "querySelector";
+    const name = "getElementById";
+  
+    document.querySelector = function (...args) {
+      console.error("Unauthorized");
+    };
+    
+    document.getElementById = function (...args) {
+      consol
+        e.error("Unauthorized");   
+    };
+    document.elementFromPoint = function (...args) {
+      console.error("Unauthorized");  
+    };
+  
+    document.appendChild = function (...args) { 
+      console.error("Unauthorized");  
+    };
+  
+    document.getElementsByTagName = function (...args) {
+      console.error("Unauthorized");  
+    };
+  
+    //Headless browsers.
+    navigator.permissions
+      .query({ name: "notifications" })
+      .then(function (permissionStatus) {
+        if (
+          Notification.permission === "denied" &&
+          permissionStatus.state === "prompt"
+        ) {
+          console.error("Unauthorized");	
+        }
+      });
+  
+    window.innerWidth = 22;
+    window.innerHeight = 33;
+    window.devicePixelRatio = 2;
+  },2000)
 
 }
 
